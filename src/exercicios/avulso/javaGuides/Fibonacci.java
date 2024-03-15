f618fa8c55234345a2b3fffc43fee08f6d93b53e
rodrigo.santos <rodmag@outlook.com>
2024-03-15 10:25:42 -0300

    Adiconar arquivo Fibonacci

---
+++ src/exercicios/avulso/javaGuides/Fibonacci.java
@@ -0,0 +1,22 @@
+package exercicios.avulso.javaGuides;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o número para calcular Fibonacci: ");
+        int count = sc.nextInt();
+
+        int firstTerm = 0;
+        int secondTerm = 1;
+
+        for(int i = 1; i<=count; i++){
+            System.out.println(firstTerm + " " );
+
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
+}
